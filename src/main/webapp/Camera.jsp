66fdd33f7fedc0e7b8903dbc99d9d9a40d7b89e3
Tolis <silot01@yahoo.gr>
2015-11-13 19:29:03 +0200

    Enable_Camera
    Ο χρήσης έχει την δυνατότητα να βγάλει φοτογραφεια να φενεται στην
    εφαρμογή ή να την κατεβάσει στον υπολογιστή του .

---
+++ src/main/webapp/Camera.jsp
@@ -0,0 +1,15 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    
+    <head>
+        
+    </head>
+    
+    <body>
+      <%@ include file="page.html" %>
+    </body>
+    
+</html>
---
+++ src/main/webapp/cameraScript.js
@@ -0,0 +1,48 @@
+
+
+		/* global Canvas2Image */
+
+// arxikopiish twn listeners
+		window.addEventListener("DOMContentLoaded", function() {
+			// Grab elements, create settings, etc.
+			var canvas = document.getElementById("canvas"),
+				context = canvas.getContext("2d"),
+				video = document.getElementById("video"),
+				videoObj = { "video": true },
+				errBack = function(error) {
+					console.log("Video capture error: ", error.code); 
+				};
+
+			// video listeners 
+			if(navigator.getUserMedia) { // Standard
+				navigator.getUserMedia(videoObj, function(stream) {
+					video.src = stream;
+					video.play();
+				}, errBack);
+			} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+				navigator.webkitGetUserMedia(videoObj, function(stream){
+					video.src = window.webkitURL.createObjectURL(stream);
+					video.play();
+				}, errBack);
+			} else if(navigator.mozGetUserMedia) { // WebKit-prefixed
+				navigator.mozGetUserMedia(videoObj, function(stream){
+					video.src = window.URL.createObjectURL(stream);
+					video.play();
+				}, errBack);
+			}
+
+			// trigger gia lipsi fotografias
+			document.getElementById("snap").addEventListener("click", function() {
+				context.drawImage(video, 0, 0, 320, 240); 
+                                
+                        });           
+                        
+                        
+                }, false);
+                
+
+
+
+
+
+
